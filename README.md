c353e15cef568efe851fc184312c0d68b9b9b248
Glitch (javascript-dropdown-navigation) <none>
2018-12-12 10:55:18 +0000

    💒🎅 Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# javascript-dropdown-navigation
-Create a dropdown menu that follows the mouse along the top. Tutorial 26 from Wes Bos Javascript30
+javascript-dropdown-navigation
+=================
+
+A dropdown navigation menu that follows the mouse along each list item in the top. The size of the background of the dropdown adjusts to the size of the content menu. 
+
+Notes
+-----------
+
+Uses direct descendants, mouse events and event listeners. An ES6 function () uses this from parent function. 
+
+Link to: [Wes Bos Youtube Javascript30 tutorial 27](https://www.youtube.com/watch?v=C9EWifQ5xqA&list=PLu8EoSxDXHP6CGK4YVJhL_VWetA865GOH&index=27)
---
+++ script.js
@@ -0,0 +1,33 @@
+const triggers = document.querySelectorAll('.cool > li'); //all direct descendants of the class cool that are li
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+function handleEnter() {
+  this.classList.add('trigger-enter');
+  //with arrow function, value of this is inherited from parent
+  setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+  background.classList.add('open');
+  
+  const dropdown = this.querySelector('.dropdown'); //different dropdown under each list item
+  const dropdownCoords = dropdown.getBoundingClientRect(); //gets dimensions of dropdown rect
+  const navCoords = nav.getBoundingClientRect(); //in case header added etc that would push the whole menu position down
+  
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left  
+  };
+  
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`); //css property
+}
+
+function handleLeave() {
+  this.classList.remove('trigger-enter', 'trigger-enter-active');
+  background.classList.remove('open');
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
